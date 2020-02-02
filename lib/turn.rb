270230dd62191578ee460c5539e0357c406674ea
Solomon Chu <solomonchu@live.com>
2020-02-02 18:13:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,17 @@
+def valid_move?(board, index)
+index.is_a?(Integer) && index.between?(0,8) && !position_taken?(board,index)
+end
+
+def position_taken?(board, index)
+  ((board[index] == "X") || (board[index] == "O"))
+end
+
 def display_board(board)
-  puts "["#{board[]",",
-end
-
-def valid_move(move)
-end
-
-def position_taken?(position)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def move(board)
