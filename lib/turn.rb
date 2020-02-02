2af4fc75e49f55d6a1117d6accaba3809097ac16
Solomon Chu <solomonchu@live.com>
2020-02-02 18:12:11 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def display_board(board)
+  puts "["#{board[]",",
+end
+
+def valid_move(move)
+end
+
+def position_taken?(position)
+end
+
+def move(board)
+  input = gets.strip
+  input.to_i-1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+end
